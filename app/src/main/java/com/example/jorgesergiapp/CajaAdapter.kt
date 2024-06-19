f2f7df5142e46ab3636178d7a757fa71a8d20c6a
sbertran <bigres24@gmail.com>
2024-06-19 22:57:10 +0200

    Cambios app

---
+++ app/src/main/java/com/example/jorgesergiapp/CajaAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.jorgesergiapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class PokemonAdapter(private val listaUsuarios: List<detalle.pokemon>) :
+    RecyclerView.Adapter<PokemonAdapter.UsuarioViewHolder>() {
+
+    class UsuarioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageViewFoto: ImageView = itemView.findViewById(R.id.imageViewFoto)
+        val textViewNombre: TextView = itemView.findViewById(R.id.textViewNombre)
+        val textViewDescripcion: TextView = itemView.findViewById(R.id.textViewDescripcion)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsuarioViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_pokemon, parent, false)
+        return UsuarioViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: UsuarioViewHolder, position: Int) {
+        val usuario = listaUsuarios[position]
+        holder.imageViewFoto.setImageResource(usuario.foto)
+        holder.textViewNombre.text = usuario.nombre
+        holder.textViewDescripcion.text = usuario.descripcion
+    }
+
+    override fun getItemCount(): Int {
+        return listaUsuarios.size
+    }
+}
