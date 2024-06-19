277f44ee06ab5e4182032ff6ec0e7230a37efa1e
JorgeIs-01 <72407493+JorgeIs-01@users.noreply.github.com>
2024-06-19 07:04:04 +0200

    Update loginApp.kt

--- app/src/main/java/com/example/jorgesergiapp/loginApp.kt
+++ app/src/main/java/com/example/jorgesergiapp/loginApp.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.example.jorgesergiapp.models.Usuario
+import com.google.firebase.firestore.FirebaseFirestore
 
 class loginApp : AppCompatActivity() {
     object UserManager {
@@ -13,36 +15,47 @@ class loginApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        main();
+        val db = FirebaseFirestore.getInstance()
+        //**BORRAR** main();
         val buttonClick = findViewById<Button>(R.id.button)
         buttonClick.setOnClickListener {
             val usuarioEditText = findViewById<EditText>(R.id.editTextTextEmailAddress2)
-            val usuarioObjeto = Usuario(usuarioEditText.text.toString(), "",0,0)
+            val usuarioPass = findViewById<EditText>(R.id.editTextTextPassword)
+
+            //**BORRAR** val usuarioObjeto = Usuario(usuarioEditText.text.toString(), "",0,0)
             // Guardar información del usuario en el Singleton
-            UserManager.usuario = usuarioObjeto
+            // UserManager.usuario = usuarioObjeto
+            // Consulta la colección "Entrenador" para el correo proporcionado
+            db.collection("Entrenador")
+                .whereEqualTo("Correo", usuarioEditText.text.toString())
+                .get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        val storedPassword = document.getString("contraseña")
+                        if (storedPassword == usuarioPass.text.toString()) {
+                            // Contraseña coincide, redirige al homeApp
+                            val intent = Intent(this, homeApp::class.java)
+                            startActivity(intent)
+                            return@addOnSuccessListener
+                        }
+                    }
+                    // Si no se encuentra un usuario coincidente, muestra un mensaje de error
+                    Toast.makeText(this, "Correo o contraseña incorrectos", Toast.LENGTH_SHORT)
+                        .show()
+                    val intent = Intent(this, homeApp::class.java)
+                    startActivity(intent)
 
-            val intent = Intent(this, homeApp::class.java)
-            startActivity(intent)
-        }
-        val buttonClickRegister = findViewById<Button>(R.id.button3)
-        buttonClickRegister.setOnClickListener {
-            val intent = Intent(this, register::class.java)
-            startActivity(intent)
+                }
+            val buttonClickRegister = findViewById<Button>(R.id.button3)
+            buttonClickRegister.setOnClickListener {
+                val intent = Intent(this, register::class.java)
+                startActivity(intent)
+            }
+
+
         }
 
 
 
     }
-
-
-
-
-
-
-
-    fun main() {
-
-    }
-
-
 }
